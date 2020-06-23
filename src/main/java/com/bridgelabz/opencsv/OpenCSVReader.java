fff73be79a5414240db72d30ed05230f69693d9c
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-24 03:16:22 +0530

    Added: read all record at once into list

--- src/main/java/com/bridgelabz/opencsv/OpenCSVReader.java
+++ src/main/java/com/bridgelabz/opencsv/OpenCSVReader.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "./users.csv";
@@ -15,14 +16,15 @@ public class OpenCSVReader {
                 Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
                 CSVReader csvReader = new CSVReader(reader);
         ) {
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-                System.out.println("Name : " + nextRecord[0]);
-                System.out.println("Email : " + nextRecord[1]);
-                System.out.println("Phone : " + nextRecord[2]);
-                System.out.println("Country : " + nextRecord[3]);
-                System.out.println("==========================");
+            List<String[]> records = csvReader.readAll();
+            //Read all records at once.
+            for (String[] record : records) {
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
+                System.out.println("---------------------------");
+            }
             }
         }
-    }
 }
